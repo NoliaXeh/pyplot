ce8fae87e202b36857192cb4e3f1be17a127d5d4
Nolia__ <thibault.marchal@epita.fr>
2024-03-03 08:32:32 +0100

    Add doc for action overload error exception and template action decorator

--- pyplot/action.py
+++ pyplot/action.py
@@ -2,10 +2,18 @@ import jinja2
 from .pyplot import Message, Plot
 
 class ActionOverloadError(Exception):
+    """
+    Exception raised when an action overload is encountered.
+
+    Attributes:
+        obj (type): The object that the action belongs to.
+        function (str): The name of the function that was not implemented.
+    """
+
     def __init__(self, obj, function):
         self.obj = obj
         self.function = function
-    
+
     def __str__(self):
         return f"Action {self.obj.__name__}.{self.function} was not implemented."
 
@@ -13,6 +21,15 @@ __plot_actions = {}
 def action(classDefinition):
     """
     Decorator to register a class action and check if it was overloaded.
+
+    Args:
+        classDefinition: The class to be registered as an action.
+
+    Raise:
+        ActionOverloadError: If the class does not have the required methods 'trigger' and 'execute'.
+
+    Returns:
+        The registered class.
     """
     def check_method(name):
         if not hasattr(classDefinition, name):
@@ -25,6 +42,21 @@ def action(classDefinition):
 
 __templates = {}
 def template_action(classDefinition):
+    """
+    Applies a template-based action to a plot.
+    It looks for a "template" attribute in the class definition and uses it to render the message.
+    It also looks for a "message" attribute in the class definition and uses it to match the message title.
+
+    Args:
+        classDefinition: The class definition containing the action details.
+
+    Returns:
+        The action object.
+
+    Raises:
+        None.
+    """
+
     if not hasattr(classDefinition, 'trigger'):
         if hasattr(classDefinition, 'message'):
             message_title = classDefinition.message
@@ -46,7 +78,13 @@ def template_action(classDefinition):
 
 def play(plot: Plot):
     """
-    Execute the actions for the given plot.
+    Executes the actions defined in the plot for each message.
+
+    Args:
+        plot (Plot): The plot containing the messages and actions.
+
+    Returns:
+        str: The result of executing the actions.
     """
     environment = jinja2.Environment(loader=jinja2.FileSystemLoader("./"))
     for template_name in __templates:
